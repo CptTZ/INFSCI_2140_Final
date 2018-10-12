ea973b570337dd2554dd74d7b294f7e9a1817074
CptTony <i@cpttony.me>
2018-10-12 03:04:47 -0400

    Hello world main page.

--- src/main/java/pitt/infsci2140/finalprj/FinalApplication.java
+++ src/main/java/pitt/infsci2140/finalprj/FinalApplication.java
@@ -1,12 +1,30 @@
 package pitt.infsci2140.finalprj;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
-public class FinalApplication {
+public class FinalApplication implements ApplicationRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(FinalApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(FinalApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/New_York"));
+        if (args.getSourceArgs().length > 0)
+            logger.warn("Command-line arguments: {}", String.join(",", args.getSourceArgs()));
+        for (String name : args.getOptionNames()) {
+            logger.info(String.format("Non-option->%s=%s", name, args.getOptionValues(name)));
+        }
     }
 }
