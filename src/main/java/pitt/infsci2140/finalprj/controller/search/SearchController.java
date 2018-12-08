b962063388c05f76156b34adef3a4f07d2ea1a2d
CptTony <i@cpttony.me>
2018-12-08 13:43:08 -0500

    Multi search

--- src/main/java/pitt/infsci2140/finalprj/controller/search/SearchController.java
+++ src/main/java/pitt/infsci2140/finalprj/controller/search/SearchController.java
@@ -55,6 +55,7 @@ public class SearchController {
         }
         model.setViewName("searchResult");
         model.addObject("results", srb);
+        model.addObject("retS", search);
         return model;
     }
 
